f39888fa614362545bfdbf3d497a9a40b1c27bc8
KARTHIK.R <155284320+karthivisva@users.noreply.github.com>
2025-03-03 12:01:43 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -66,7 +66,7 @@ const Contact = () => {
           className="bg-gray-800 p-6 rounded-lg shadow-md w-full max-w-md"
         >
           <h3 className="text-2xl font-semibold text-white mb-4">Send Us a Message</h3>
-          <form action="https://getform.io/f/awnqwpzb" method="POST" className="space-y-4">
+          <form action="https://getform.io/f/ajjmokma" method="POST" className="space-y-4">
             {[
               { type: "text", name: "name", placeholder: "Your Name" },
               {
