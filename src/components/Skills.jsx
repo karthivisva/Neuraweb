3bf3737afff84b8ed4841f658e40af0cf437b749
KARTHIK.R <155284320+karthivisva@users.noreply.github.com>
2025-03-07 22:37:21 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -32,7 +32,6 @@ const skills = [
       { name: "Node Js", icon: <DiNodejsSmall className="text-green-500" /> },
       { name: "MongoDB", icon: <DiMongodb className="text-green-600" /> },
       { name: "React", icon: <DiReact className="text-blue-500" /> },
-      { name: "GitHub", icon: <DiGithubBadge className="text-gray-600" /> },
     ],
   },
 ];
